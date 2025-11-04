9a436ad57de433f902fc7e13b1885200cf654541
Henrik Finsberg <henriknf@simula.no>
2025-11-04 22:02:03 +0100

    Use iterative solver for brain mesh

--- poisson_brain.py
+++ poisson_brain.py
@@ -5,10 +5,9 @@ import dolfinx
 from dolfinx.fem.petsc import LinearProblem
 import pyvista
 
-with dolfinx.io.XDMFFile(MPI.COMM_WORLD, "brain.xdmf", "r") as xdmf:
+with dolfinx.io.XDMFFile(MPI.COMM_WORLD, "brain_smooth.xdmf", "r") as xdmf:
     mesh = xdmf.read_mesh(name="Mesh")
     # ct = xdmf.read_meshtags(mesh, name="Mesh", attribute_name="Cell markers")
-
 
 V = dolfinx.fem.functionspace(mesh, ("Lagrange", 1))
 tdim = mesh.topology.dim
@@ -16,6 +15,15 @@ fdim = tdim - 1
 mesh.topology.create_connectivity(fdim, tdim)
 
 boundary_facets = dolfinx.mesh.exterior_facet_indices(mesh.topology)
+# boundary_facets = dolfinx.mesh.locate_entities_boundary(mesh, 2, lambda x: x[2] < 70)
+
+ft = dolfinx.mesh.meshtags(
+    mesh, fdim, boundary_facets, np.full(len(boundary_facets), 1, dtype=np.int32)
+)
+with dolfinx.io.XDMFFile(MPI.COMM_WORLD, "brain_boundaries.xdmf", "w") as xdmf:
+    xdmf.write_mesh(mesh)
+    xdmf.write_meshtags(ft, mesh.geometry)
+
 boundary_dofs = dolfinx.fem.locate_dofs_topological(V, fdim, boundary_facets)
 bc = dolfinx.fem.dirichletbc(np.array((0.0), dtype=np.float64), boundary_dofs, V=V)
 
@@ -27,13 +35,21 @@ v = ufl.TestFunction(V)
 a = ufl.dot(ufl.grad(u), ufl.grad(v)) * ufl.dx
 L = f * v * ufl.dx
 
+petsc_options = {
+    "ksp_type": "cg",
+    "ksp_rtol": 1e-08,
+    "ksp_atol": 1e-10,
+    "ksp_max_it": 10000,
+    "pc_type": "gamg",
+    "ksp_monitor": None,
+}
 
 print("Assembling system...")
 problem = LinearProblem(
     a,
     L,
     bcs=[bc],
-    petsc_options={"ksp_type": "preonly", "pc_type": "lu"},
+    petsc_options=petsc_options,
     petsc_options_prefix="Poisson",
 )
 print("Solving system...")
@@ -49,6 +65,6 @@ u_grid = pyvista.UnstructuredGrid(u_topology, u_cell_types, u_geometry)
 u_grid.point_data["u"] = uh.x.array.real
 u_grid.set_active_scalars("u")
 u_plotter = pyvista.Plotter(off_screen=True)
-u_plotter.add_mesh_clip_plane(u_grid, show_edges=True, crinkle=True, normal="-z")
+u_plotter.add_mesh_clip_plane(u_grid, origin=(96.5, 116.5, 171.5), normal="-z")
 u_plotter.view_xy()
 u_plotter.screenshot("poisson_brain.png")
